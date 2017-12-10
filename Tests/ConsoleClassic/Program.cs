671bf394f0fd20dde3043a1635110df4b996743e
Val <vtortola@gmail.com>
2017-12-10 22:24:47 +0100

    Improved testing

--- Tests/ConsoleClassic/Program.cs
+++ Tests/ConsoleClassic/Program.cs
@@ -33,7 +33,7 @@ namespace ConsoleClassic
                 var write = sw.ElapsedMilliseconds;
                 sw.Reset();
                 Console.WriteLine($"IT:{counter}, gen:{gen:0.00}, write:{write:0.00}");
-                Thread.Sleep(100);
+                Thread.Sleep(1000);
             }
             Console.WriteLine("End");
             Console.ReadKey();
@@ -42,19 +42,37 @@ namespace ConsoleClassic
         static Random ran = new Random(); // no seed
         private static string RandomMessage()
         {
-            var builder = new StringBuilder();
+            var builder = new StringBuilder("<START>");
 
-            var length = ran.Next(10, 10000);
+            var length = ran.Next(10, 1000);
             for (int i = 0; i < length; i++)
             {
-                builder.Append((char)ran.Next(32, 128));
-                if (i % 47 == 0)
+                var charSelector = ran.Next(0, 3);
+                switch (charSelector)
+                {
+                    case 0:
+                        builder.Append((char)ran.Next(1024, 1280)); // cyrillic
+                        break;
+                    case 1:
+                        builder.Append((char)ran.Next(12352, 12448)); // japanese
+                        break;
+                    default:
+                        var c = ran.Next(32, 128);
+                        c = c == 34 ? 35 : c; // avoid double quotation mark
+                        builder.Append((char)c);
+                        break;
+                }
+                
+                if(i % 7 == 0)
+                    builder.Append(" ");
+                else if (i % 13 == 0)
                     builder.Append("\r\n");
-                else if (i % 43 == 0)
+                else if (i % 23 == 0)
                     builder.Append("\n");
-                else if (i % 41 == 0)
+                else if (i % 42 == 0)
                     builder.Append("\r");
             }
+            builder.Append(" <END>");
             return builder.ToString();
         }
     }
